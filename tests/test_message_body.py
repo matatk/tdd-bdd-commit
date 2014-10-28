0b601dd3b45fa5f59cd84c062bb8b3ba0374d030
Matthew Tylee Atkinson <matatk@gmail.com>
2014-10-28 19:08:12 +0000

    RED Messages containing double quotes should be wrapped in singles.

--- tests/test_message_body.py
+++ tests/test_message_body.py
@@ -3,3 +3,7 @@ from tddcommitmessage.messagebody import MessageBody
 def test_message_wrapped_in_quotes():
     msg = MessageBody('forty-two')
     assert str(msg) == '"forty-two"'
+
+def test_message_with_double_quotes_is_wrapped_with_single():
+    msg = MessageBody('But what are "Birds"?')
+    assert str(msg) == r"""'But what are "Birds"?'"""
