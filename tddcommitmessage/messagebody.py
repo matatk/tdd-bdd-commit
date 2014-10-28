de46a9e4343ad0e1e5b8940f1e5333a0c815a3fc
Matthew Tylee Atkinson <matatk@gmail.com>
2014-10-28 21:02:45 +0000

    GREEN

--- tddcommitmessage/messagebody.py
+++ tddcommitmessage/messagebody.py
@@ -1,7 +1,11 @@
 class MessageBody:
     def __init__(self, kind, message):
-        self._message = message
+        self._message = self._capitalise_first(message)
         self._kind = kind
+
+    @staticmethod
+    def _capitalise_first(string):
+        return string[0].upper() + string[1:]
 
     def __str__(self):
         if '"' in self._message:
--- tests/test_message_body.py
+++ tests/test_message_body.py
@@ -8,3 +8,7 @@ def test_message_is_wrapped_in_quotes():
 def test_message_with_double_quote_is_wrapped_with_single():
     msg = MessageBody(Kind.red, 'But what are "Birds"?')
     assert str(msg) == r"""'RED But what are "Birds"?'"""
+
+def test_first_letter_capitalised():
+    msg = MessageBody(Kind.red, 'forty-two')
+    assert str(msg) == '"RED Forty-two"'
