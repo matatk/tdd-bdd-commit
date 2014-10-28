85d12c7ce38c4349a98ccd09266bc3f08f511daa
Matthew Tylee Atkinson <matatk@gmail.com>
2014-10-28 19:12:19 +0000

    REFACTOR Improve test name's grammar and accuracy.

--- tests/test_message_body.py
+++ tests/test_message_body.py
@@ -1,9 +1,9 @@
 from tddcommitmessage.messagebody import MessageBody
 
-def test_message_wrapped_in_quotes():
+def test_message_is_wrapped_in_quotes():
     msg = MessageBody('forty-two')
     assert str(msg) == '"forty-two"'
 
-def test_message_with_double_quotes_is_wrapped_with_single():
+def test_message_with_double_quote_is_wrapped_with_single():
     msg = MessageBody('But what are "Birds"?')
     assert str(msg) == r"""'But what are "Birds"?'"""
