fb6e5b11492675b7a7c94424737c91acbb541d69
Matthew Tylee Atkinson <matatk@gmail.com>
2014-10-28 21:03:19 +0000

    REFACTOR Change order of tests.

--- tests/test_message_body.py
+++ tests/test_message_body.py
@@ -5,10 +5,10 @@ def test_message_is_wrapped_in_quotes():
     msg = MessageBody(Kind.red, 'Forty-two')
     assert str(msg) == '"RED Forty-two"'
 
-def test_message_with_double_quote_is_wrapped_with_single():
-    msg = MessageBody(Kind.red, 'But what are "Birds"?')
-    assert str(msg) == r"""'RED But what are "Birds"?'"""
-
 def test_first_letter_capitalised():
     msg = MessageBody(Kind.red, 'forty-two')
     assert str(msg) == '"RED Forty-two"'
+
+def test_message_with_double_quote_is_wrapped_with_single():
+    msg = MessageBody(Kind.red, 'But what are "Birds"?')
+    assert str(msg) == r"""'RED But what are "Birds"?'"""
