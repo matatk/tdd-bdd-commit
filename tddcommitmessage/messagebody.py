d69abb38e66ddf62910704192b84a68ce1b18a7e
Matthew Tylee Atkinson <matatk@gmail.com>
2014-10-28 20:58:29 +0000

    REFACTOR Include kind in message body tests.

--- tddcommitmessage/messagebody.py
+++ tddcommitmessage/messagebody.py
@@ -1,9 +1,10 @@
 class MessageBody:
-    def __init__(self, message):
+    def __init__(self, kind, message):
         self._message = message
+        self._kind = kind
 
     def __str__(self):
         if '"' in self._message:
-            return "'" + self._message + "'"
+            return "'" + self._kind + ' ' + self._message + "'"
         else:
-            return '"' + self._message + '"'
+            return '"' + self._kind + ' ' + self._message + '"'
--- tests/test_message_body.py
+++ tests/test_message_body.py
@@ -1,9 +1,10 @@
 from tddcommitmessage.messagebody import MessageBody
+from tddcommitmessage import Kind
 
 def test_message_is_wrapped_in_quotes():
-    msg = MessageBody('forty-two')
-    assert str(msg) == '"forty-two"'
+    msg = MessageBody(Kind.red, 'Forty-two')
+    assert str(msg) == '"RED Forty-two"'
 
 def test_message_with_double_quote_is_wrapped_with_single():
-    msg = MessageBody('But what are "Birds"?')
-    assert str(msg) == r"""'But what are "Birds"?'"""
+    msg = MessageBody(Kind.red, 'But what are "Birds"?')
+    assert str(msg) == r"""'RED But what are "Birds"?'"""
