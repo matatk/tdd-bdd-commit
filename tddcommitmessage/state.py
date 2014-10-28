e18761cc6bddfb63d4fcb951567b4f1e40a954ad
Matthew Tylee Atkinson <matatk@gmail.com>
2014-10-28 19:33:16 +0000

    REFACTOR Clearer state (parameter) names.

--- tddcommitmessage/state.py
+++ tddcommitmessage/state.py
@@ -4,15 +4,15 @@ class State:
     def __init__(self):
         self._current_state = None
 
-    def allowed(self, next_state):
+    def allowed(self, proposed_state):
         if not self._current_state:
-            if next_state == Kinds.initial:
+            if proposed_state == Kinds.initial:
                 return True
         elif self._current_state == Kinds.initial:
-            if next_state == Kinds.red:
+            if proposed_state == Kinds.red:
                 return True
         return False
 
-    def change(self, state):
-        if self.allowed(state):
-            self._current_state = state
+    def change(self, to_state):
+        if self.allowed(to_state):
+            self._current_state = to_state
