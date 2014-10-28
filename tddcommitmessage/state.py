032043efbad0bf034fa61149e6c4c266ccb31cf1
Matthew Tylee Atkinson <matatk@gmail.com>
2014-10-28 19:32:06 +0000

    GREEN

--- tddcommitmessage/state.py
+++ tddcommitmessage/state.py
@@ -1,10 +1,18 @@
 from tddcommitmessage import Kinds
 
 class State:
+    def __init__(self):
+        self._current_state = None
+
     def allowed(self, next_state):
-        if next_state == Kinds.initial:
-            return True
+        if not self._current_state:
+            if next_state == Kinds.initial:
+                return True
+        elif self._current_state == Kinds.initial:
+            if next_state == Kinds.red:
+                return True
         return False
 
     def change(self, state):
-        pass
+        if self.allowed(state):
+            self._current_state = state
