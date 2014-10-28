886c869469614ada84bd05fc50b697e2edeafa9c
Matthew Tylee Atkinson <matatk@gmail.com>
2014-10-28 19:25:08 +0000

    REFACTOR Make assertion explicit.

--- tests/test_state_transitions.py
+++ tests/test_state_transitions.py
@@ -3,7 +3,7 @@ from tddcommitmessage import Kinds
 
 def test_initial_allowed_first():
     state = State()
-    assert state.allowed(Kinds.initial)
+    assert state.allowed(Kinds.initial) is True
 
 def test_only_initial_allowed_first():
     state = State()
