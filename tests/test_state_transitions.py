b87c35e3c7580f7a8978a415b303597e14f213e6
Matthew Tylee Atkinson <matatk@gmail.com>
2014-10-29 16:40:34 +0000

    REFACTOR DRY state transition tests.

--- tests/test_state_transitions.py
+++ tests/test_state_transitions.py
@@ -45,91 +45,71 @@ def test_green_is_allowed_after_red():
 # What is allowed after 'green'?
 #
 
-def test_refactor_is_allowed_after_green():
+@pytest.fixture
+def state_green():
     state = State()
     state.change(Kind.initial)
     state.change(Kind.red)
     state.change(Kind.green)
-    assert state.allowed(Kind.refactor) is True
+    return state
 
-def test_red_is_allowed_after_green():
-    state = State()
-    state.change(Kind.initial)
-    state.change(Kind.red)
-    state.change(Kind.green)
-    assert state.allowed(Kind.red) is True
+def test_refactor_is_allowed_after_green(state_green):
+    assert state_green.allowed(Kind.refactor) is True
 
-def test_merge_is_allowed_after_green():
-    state = State()
-    state.change(Kind.initial)
-    state.change(Kind.red)
-    state.change(Kind.green)
-    assert state.allowed(Kind.merge) is True
+def test_red_is_allowed_after_green(state_green):
+    assert state_green.allowed(Kind.red) is True
 
-def test_beige_is_allowed_after_green():
-    state = State()
-    state.change(Kind.initial)
-    state.change(Kind.red)
-    state.change(Kind.green)
-    assert state.allowed(Kind.beige) is True
+def test_merge_is_allowed_after_green(state_green):
+    assert state_green.allowed(Kind.merge) is True
+
+def test_beige_is_allowed_after_green(state_green):
+    assert state_green.allowed(Kind.beige) is True
 
 
 #
 # What is allowed after 'refactor'?
 #
 
-def test_refactor_is_allowed_after_refactor():
+@pytest.fixture
+def state_refactor():
     state = State()
     state.change(Kind.initial)
     state.change(Kind.red)
     state.change(Kind.green)
     state.change(Kind.refactor)
-    assert state.allowed(Kind.refactor) is True
+    return state
 
-def test_red_is_allowed_after_refactor():
-    state = State()
-    state.change(Kind.initial)
-    state.change(Kind.red)
-    state.change(Kind.green)
-    state.change(Kind.refactor)
-    assert state.allowed(Kind.red) is True
+def test_refactor_is_allowed_after_refactor(state_refactor):
+    assert state_refactor.allowed(Kind.refactor) is True
 
-def test_merge_is_allowed_after_refactor():
-    state = State()
-    state.change(Kind.initial)
-    state.change(Kind.red)
-    state.change(Kind.green)
-    state.change(Kind.refactor)
-    assert state.allowed(Kind.merge) is True
+def test_red_is_allowed_after_refactor(state_refactor):
+    assert state_refactor.allowed(Kind.red) is True
+
+def test_merge_is_allowed_after_refactor(state_refactor):
+    assert state_refactor.allowed(Kind.merge) is True
 
 
 #
 # What is allowed after 'merge'?
 #
 
-def test_refactor_is_allowed_after_merge():
+@pytest.fixture
+def state_merge():
     state = State()
     state.change(Kind.initial)
     state.change(Kind.red)
     state.change(Kind.green)
     state.change(Kind.merge)
-    assert state.allowed(Kind.refactor) is True
+    return state
 
-def test_red_is_allowed_after_merge():
-    state = State()
-    state.change(Kind.initial)
-    state.change(Kind.red)
-    state.change(Kind.green)
-    state.change(Kind.merge)
-    assert state.allowed(Kind.red) is True
+def test_refactor_is_allowed_after_merge(state_merge):
+    assert state_merge.allowed(Kind.refactor) is True
 
-def test_merge_is_allowed_after_merge():
-    state = State()
-    state.change(Kind.initial)
-    state.change(Kind.red)
-    state.change(Kind.green)
-    state.change(Kind.merge)
-    assert state.allowed(Kind.merge) is True
+def test_red_is_allowed_after_merge(state_merge):
+    assert state_merge.allowed(Kind.red) is True
+
+def test_merge_is_allowed_after_merge(state_merge):
+    assert state_merge.allowed(Kind.merge) is True
 
 
 #
