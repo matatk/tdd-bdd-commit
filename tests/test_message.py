c750930956ca0dd456146e0163f1044a5f02f746
Matthew Tylee Atkinson <matatk@gmail.com>
2016-02-03 22:34:55 +0000

    REFACTOR Re-order Message tests for clarity

--- tests/test_message.py
+++ tests/test_message.py
@@ -4,6 +4,40 @@ from tddbddcommit import Kind
 
 
 #
+# Kind Validity
+#
+
+def test_invalid_kind_raises_error():
+    with pytest.raises(MessageSummaryError) as excinfo:
+        msg = Message()
+        msg.kind(42)
+    assert 'Invalid commit kind given' in str(excinfo.value)
+
+
+#
+# Message Validity
+#
+
+def test_empty_message_raises_error():
+    with pytest.raises(MessageSummaryError) as excinfo:
+        msg = Message()
+        msg.kind(Kind.red)
+        msg.summary(None)
+    assert 'No message given' in str(excinfo.value)
+
+
+def test_long_message_raises_error():
+    max_length = 75
+    max_red_length = max_length - 1 - len(Kind.red)
+    long_message = '@' * (max_red_length + 1)
+    with pytest.raises(MessageSummaryError) as excinfo:
+        msg = Message()
+        msg.kind(Kind.red)
+        msg.summary(long_message)
+    assert '1 character(s) too long' in str(excinfo.value)
+
+
+#
 # String handling
 #
 
@@ -26,38 +60,3 @@ def test_message_with_double_quote_is_wrapped_with_single():
     msg.kind(Kind.red)
     msg.summary('But what are "Birds"?')
     assert str(msg) == r"""'RED But what are "Birds"?'"""
-
-
-#
-# Message Validity
-#
-
-def test_long_message_raises_error():
-    max_length = 75
-    max_red_length = max_length - 1 - len(Kind.red)
-    long_message = '@' * (max_red_length + 1)
-    with pytest.raises(MessageSummaryError) as excinfo:
-        msg = Message()
-        msg.kind(Kind.red)
-        msg.summary(long_message)
-    assert '1 character(s) too long' in str(excinfo.value)
-
-
-def test_empty_message_raises_error():
-    with pytest.raises(MessageSummaryError) as excinfo:
-        msg = Message()
-        msg.kind(Kind.red)
-        msg.summary(None)
-    assert 'No message given' in str(excinfo.value)
-
-
-#
-# Kind Validity
-#
-
-def test_invalid_kind_raises_error():
-    with pytest.raises(MessageSummaryError) as excinfo:
-        msg = Message()
-        msg.kind(42)
-        msg.summary('42')
-    assert 'Invalid commit kind given' in str(excinfo.value)
