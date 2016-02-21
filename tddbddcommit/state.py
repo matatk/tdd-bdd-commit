80ff687ade7c008e620a90915478838025b64e29
Matthew Tylee Atkinson <matatk@gmail.com>
2016-02-21 19:05:24 +0000

    REFACTOR Make state transition code clearer
    A number of functions have been factored out, in order to keep the
    number of lines of code per function smaller, and to increase clarity
    (now the returns are all explicitly True or False).

--- tddbddcommit/state.py
+++ tddbddcommit/state.py
@@ -3,23 +3,47 @@ from tddbddcommit import Kind
 
 def allowed(current_state, proposed_state):
     if not current_state:
-        if proposed_state is Kind.initial:
-            return True
+        allowed = _allowed_from_start(proposed_state)
     elif current_state is Kind.initial:
-        if (proposed_state is Kind.red or
-           proposed_state is Kind.merge or
-           proposed_state is Kind.beige):
-            return True
+        allowed = _allowed_from_initial(proposed_state)
     elif current_state is Kind.red:
-        if proposed_state is Kind.green:
-            return True
+        allowed = _allowed_from_red(proposed_state)
     elif (current_state is Kind.green or
           current_state is Kind.refactor or
           current_state is Kind.merge or
           current_state is Kind.beige):
-        if (proposed_state is Kind.refactor or
-           proposed_state is Kind.red or
-           proposed_state is Kind.merge or
-           proposed_state is Kind.beige):
-            return True
-    return False
+        allowed = _allowed_from_green_refactor_merge_beige(proposed_state)
+    return allowed
+
+
+def _allowed_from_start(proposed_state):
+    if proposed_state is Kind.initial:
+        return True
+    else:
+        return False
+
+
+def _allowed_from_initial(proposed_state):
+    if (proposed_state is Kind.red or
+       proposed_state is Kind.merge or
+       proposed_state is Kind.beige):
+        return True
+    else:
+        return False
+
+
+def _allowed_from_red(proposed_state):
+    if proposed_state is Kind.green:
+        return True
+    else:
+        return False
+
+
+def _allowed_from_green_refactor_merge_beige(proposed_state):
+    if (proposed_state is Kind.refactor or
+       proposed_state is Kind.red or
+       proposed_state is Kind.merge or
+       proposed_state is Kind.beige):
+        return True
+    else:
+        return False
