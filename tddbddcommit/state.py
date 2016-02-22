8cb9bfc8727ef65d85e8c181b40e994e24a987e7
Matthew Tylee Atkinson <matatk@gmail.com>
2016-02-22 19:41:33 +0000

    REFACTOR State transitions
    * Massively simplify the code (thanks @MarkWithall :-))
    * Ensure that the tests are sufficiently comprehensive (the “only” bit)
    
    Note: this is probably a little overstepping what a refactoring should
          be, but it doesn't change the behaviour from what was *intended*
          -- but it *does* change the behaviour from what it *was*, so maybe
          next time I shan't do it in such a single big step!

--- tddbddcommit/state.py
+++ tddbddcommit/state.py
@@ -3,47 +3,9 @@ from tddbddcommit import Kind
 
 def allowed(current_state, proposed_state):
     if not current_state:
-        allowed = _allowed_from_start(proposed_state)
-    elif current_state is Kind.initial:
-        allowed = _allowed_from_initial(proposed_state)
-    elif current_state is Kind.red:
-        allowed = _allowed_from_red(proposed_state)
-    elif (current_state is Kind.green or
-          current_state is Kind.refactor or
-          current_state is Kind.merge or
-          current_state is Kind.beige):
-        allowed = _allowed_from_green_refactor_merge_beige(proposed_state)
-    return allowed
-
-
-def _allowed_from_start(proposed_state):
-    if proposed_state is Kind.initial:
-        return True
-    else:
-        return False
-
-
-def _allowed_from_initial(proposed_state):
-    if (proposed_state is Kind.red or
-       proposed_state is Kind.merge or
-       proposed_state is Kind.beige):
-        return True
-    else:
-        return False
-
-
-def _allowed_from_red(proposed_state):
-    if proposed_state is Kind.green:
-        return True
-    else:
-        return False
-
-
-def _allowed_from_green_refactor_merge_beige(proposed_state):
-    if (proposed_state is Kind.refactor or
-       proposed_state is Kind.red or
-       proposed_state is Kind.merge or
-       proposed_state is Kind.beige):
-        return True
-    else:
-        return False
+        return proposed_state is Kind.initial
+    if current_state is Kind.initial:
+        return proposed_state in [Kind.red, Kind.merge, Kind.beige]
+    if current_state is Kind.red:
+        return proposed_state is Kind.green
+    return proposed_state not in [Kind.initial, Kind.green]
--- tests/test_state_transitions.py
+++ tests/test_state_transitions.py
@@ -32,9 +32,11 @@ def test_green_is_allowed_after_red():
     assert allowed(Kind.red, Kind.green) is True
 
 
-# Common test for when Red, Refactor, Merge and Beige should be allowed
+# Common test for when only Red, Refactor, Merge and Beige should be allowed
 
-def _check_red_refactor_merge_beige_states_are_allowed(current):
+def _check_only_red_refactor_merge_beige_states_are_allowed(current):
+    assert allowed(current, Kind.initial) is False
+    assert allowed(current, Kind.green) is False
     assert allowed(current, Kind.red) is True
     assert allowed(current, Kind.refactor) is True
     assert allowed(current, Kind.merge) is True
@@ -44,22 +46,22 @@ def _check_red_refactor_merge_beige_states_are_allowed(current):
 # What is allowed after 'green'?
 
 def test_states_allowed_after_green():
-    _check_red_refactor_merge_beige_states_are_allowed(Kind.green)
+    _check_only_red_refactor_merge_beige_states_are_allowed(Kind.green)
 
 
 # What is allowed after 'refactor'?
 
 def test_states_allowed_after_refactor():
-    _check_red_refactor_merge_beige_states_are_allowed(Kind.refactor)
+    _check_only_red_refactor_merge_beige_states_are_allowed(Kind.refactor)
 
 
 # What is allowed after 'merge'?
 
 def test_states_allowed_after_merge():
-    _check_red_refactor_merge_beige_states_are_allowed(Kind.merge)
+    _check_only_red_refactor_merge_beige_states_are_allowed(Kind.merge)
 
 
 # What is allowed after 'beige'?
 
 def test_states_allowed_after_beige():
-    _check_red_refactor_merge_beige_states_are_allowed(Kind.beige)
+    _check_only_red_refactor_merge_beige_states_are_allowed(Kind.beige)
